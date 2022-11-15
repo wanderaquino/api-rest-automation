e610192dc48e64c0df7642684e4c15607f9d0e4f
Wander Aquino <wander.aquino@gmail.com>
2022-11-15 20:44:41 -0300

    Add database for scenarios

---
+++ database/index.js
@@ -0,0 +1,9 @@
+const users = require("./users.json");
+const userById = require("./userById.json");
+const emptyUser = require("./emptyUser.json");
+
+module.exports = {
+    users,
+    userById,
+    emptyUser
+}
