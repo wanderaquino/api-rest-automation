99952778f6a3ceda5f9bce06548ba31236d56b02
Wander Aquino <wander.aquino@gmail.com>
2022-11-20 20:30:34 -0300

    Use newUser json database file

--- database/index.js
+++ database/index.js
@@ -1,9 +1,10 @@
 const users = require("./users.json");
 const userById = require("./userById.json");
 const emptyUser = require("./emptyUser.json");
-
+const newUser = require("./newUser.json");
 module.exports = {
     users,
     userById,
-    emptyUser
+    emptyUser,
+    newUser
 }
